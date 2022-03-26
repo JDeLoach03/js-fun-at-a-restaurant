afc8184aeb7414436a2a51204da60db3d1e2a513
Joshua DeLoach <J.DeLoachTech@gmail.com>
2022-03-26 11:43:59 -0600

    Complete meal-test.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`
+};
 
+function createMenuItem(itemName, itemPrice, itemType) {
+menuItem = {
+  name: itemName,
+  price: itemPrice,
+  type: itemType,
+};
+return menuItem;
 }
 
+function addIngredients(side, ingredients){
+    if(ingredients.includes(side) == false) {
+      ingredients.push(side)
+      return ingredients
+    }
+  };
+
+function formatPrice(dollar) {
+  return `$${dollar}`
+}
+
+function decreasePrice(price) {
+  return price * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title ,
+    ingredients:ingredients ,
+    type: menuItemType,
+  }
+return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
