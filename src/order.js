90e798a6033432f886c2913cacb0b0d42dd027ff
Joshua DeLoach <J.DeLoachTech@gmail.com>
2022-03-26 21:22:53 -0600

    Add order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,44 @@
+function takeOrder(deliveryOrders, newOrder) {
+  if (newOrder.length < 3){
+    return newOrder.push(deliveryOrders);
+  } else {
+    return deliveryOrders;
+  }
+}
+
+function refundOrder(orderNum,deliveryOrders) {
+  for (i = 0; i < deliveryOrders.length; i++) {
+    if (orderNum === deliveryOrders[i].orderNumber) {
+      return deliveryOrders.splice(i, 1);
+    }
+  }
+};
+
+//want to list all order items by name
+// this could mean accessing item names than
+
+function listItems(deliveryOrders) {
+  var entree = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    entree.push(deliveryOrders[i].item);
+  }
+  return entree.join(', ')
+}
+
+function searchOrder(deliveryOrders, orderItem) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if ( orderItem === deliveryOrders[i].item) {
+    return true
+  }
+  } return false
+
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
